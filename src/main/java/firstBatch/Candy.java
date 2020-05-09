4d26edfa3b5443246f3574accd2f2febc86349dc
X. Wang <xwang@xw-thinkpad-x220.attlocal.net>
2020-05-09 13:53:50 -0700

    candy + clone graph

--- src/main/java/firstBatch/Candy.java
+++ src/main/java/firstBatch/Candy.java
@@ -1,5 +1,7 @@
 package firstBatch;
 
+import org.junit.jupiter.api.Test;
+
 /*
  * 5/8/20
  * leetcode # 135. firstBatch.Candy
@@ -8,4 +10,65 @@ package firstBatch;
  * level: hard
  */
 public class Candy {
+/*    There are N children standing in a line. Each child is assigned a rating value.
+
+    You are giving candies to these children subjected to the following requirements:
+
+    Each child must have at least one candy.
+    Children with a higher rating get more candies than their neighbors.
+    What is the minimum candies you must give?
+
+    Example 1:
+
+    Input: [1,0,2]
+    Output: 5
+    Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+    Example 2:
+
+    Input: [1,2,2]
+    Output: 4
+    Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+    The third child gets 1 candy because it satisfies the above two conditions.*/
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+
+        int prevEffective = 0; // if prev increase by 1, how many need to follow to increase
+        int prevCandy = 1; // the amount of candies the prev has
+        int totalCandy = ratings.length;
+
+        for (int i = 1; i < ratings.length; i++) {
+            //[1,2,87,87,87,2,1]
+            //[1,2,3, 1, 1, 1, 1]
+            if (ratings[i] < ratings[i-1]) { // when cur rating is not greater then the prev
+                if (prevCandy > 1) { // check if prev's candy is already greater then cur
+                    int diff = prevCandy - 1; // if cur increase diff times then it will effect prev, now is safe
+                    int safeIncrease = diff - 1;
+                    // update prevEff
+                    prevEffective = prevEffective + 1  - safeIncrease; // prevEffective + cur - safeIncrease
+                } else { // need to increase the prev and its effective for sure
+                    totalCandy += prevEffective + 1; // increase prev by 1 and all its effective by 1
+                    prevEffective = prevEffective + 1;
+                }
+                prevCandy = 1;
+            } else if (ratings[i] > ratings[i-1]) { // when cur rating is greater then the prev
+                prevEffective = 0;
+                int curCandy = prevCandy + 1;
+                totalCandy += prevCandy;
+                prevCandy = curCandy;
+            } else { // when cur rating is equal then the prev
+                prevEffective = 0;
+                prevCandy = 1;
+            }
+        }
+        return totalCandy;
+    }
+
+    @Test
+    public void test() {
+        Candy candy = new Candy();
+        int result = candy.candy(new int[] {1,2,87,87,87,2,1});
+        //[1,2,87,87,87,2,1]
+        //[1,2,3, 1, 3, 2, 1]
+        System.out.println(result);
+    }
 }
--- src/main/java/firstBatch/CloneGraph.java
+++ src/main/java/firstBatch/CloneGraph.java
@@ -1,5 +1,10 @@
 package firstBatch;
 
+import utils.Node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /*
  * 5/8/20
  * leetcode # 133. Clone Graph
@@ -8,4 +13,45 @@ package firstBatch;
  * level: medium
  */
 public class CloneGraph {
+
+    /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        HashMap<Node, Node> map = new HashMap<>();
+        return dfs(node, map);
+    }
+
+    private Node dfs(Node node, HashMap<Node, Node> map) {
+        Node copy = map.get(node);
+        if (copy == null) {
+            copy = new Node(node.val);
+            copy.neighbors = new ArrayList<>();
+            map.put(node, copy);
+            for (Node n : node.neighbors) {
+                copy.neighbors.add(dfs(n, map));
+            }
+        }
+        return copy;
+    }
 }

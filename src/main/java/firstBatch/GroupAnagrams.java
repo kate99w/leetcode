cd69cb597d36f26bbe47db5b58f4672fe76dbc5f
X. Wang <xwang@xw-thinkpad-x220.attlocal.net>
2020-05-12 00:29:15 -0700

    group anagram

--- src/main/java/firstBatch/GroupAnagrams.java
+++ src/main/java/firstBatch/GroupAnagrams.java
@@ -1,5 +1,9 @@
 package firstBatch;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 /*
  * 5/8/20
  * leetcode # 49. Group Anagrams
@@ -8,4 +12,42 @@ package firstBatch;
  * level: medium
  */
 public class GroupAnagrams {
+/*    Given an array of strings, group anagrams together.
+
+            Example:
+
+    Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+    Output:
+            [
+            ["ate","eat","tea"],
+            ["nat","tan"],
+            ["bat"]
+            ]
+    Note:
+
+    All inputs will be in lowercase.
+    The order of your output does not matter.*/
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] charArray = new char[26];
+            for (int i = 0; i < s.length(); i++) {
+                charArray[s.charAt(i) - 'a']++;
+            }
+            List<String> anagrams = map.getOrDefault(Arrays.toString(charArray), new ArrayList<>());
+            anagrams.add(s);
+            map.put(Arrays.toString(charArray), anagrams);
+        }
+        List<List<String>> res = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(this.groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
 }
--- src/main/java/firstBatch/InsertInterval.java
+++ src/main/java/firstBatch/InsertInterval.java
@@ -8,4 +8,31 @@ package firstBatch;
  * level: hard
  */
 public class InsertInterval {
+/*    Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+    You may assume that the intervals were initially sorted according to their start times.
+
+    Example 1:
+
+    Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+    Output: [[1,5],[6,9]]
+    Example 2:
+
+    Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+    Output: [[1,2],[3,10],[12,16]]
+    Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+    NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.*/
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int[][] res = new int[intervals.length + 1][2];
+        int pos = 0;
+        boolean inserted = false;
+        for (int[] ints : intervals) {
+            if (!inserted) {
+                if (newInterval[1] < ints[0]) {
+
+                }
+            }
+        }
+    }
 }

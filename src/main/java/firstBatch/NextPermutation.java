f65c5877be28c27ed7e23d6c7e6e35ee3b795157
kate99w <kate99w@gmail.com>
2020-05-16 12:21:29 -0700

    next permutation + palindrompartioning I II

--- src/main/java/firstBatch/NextPermutation.java
+++ src/main/java/firstBatch/NextPermutation.java
@@ -8,4 +8,22 @@ package firstBatch;
  * level: medium
  */
 public class NextPermutation {
+/*    Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+    If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+    The replacement must be in-place and use only constant extra memory.
+
+    Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+            1,2,3 → 1,3,2
+            1, 3,2,1 → 1,2,3
+            1,1,5 → 1,5,1
+
+            3, 2,7,4, 3,6, 5,4,1
+
+*/
+
+    public void nextPermutation(int[] nums) {
+    }
 }

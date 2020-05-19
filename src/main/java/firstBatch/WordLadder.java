2ae0275d8c8fd0e2dc04afa5515d29a6ba62cfee
kate99w <kate99w@gmail.com>
2020-05-18 21:32:51 -0700

    word ladder

--- src/main/java/firstBatch/WordLadder.java
+++ src/main/java/firstBatch/WordLadder.java
@@ -64,8 +64,8 @@ public class WordLadder {
         Map<String, List<String>> dic = new HashMap<>();
         for (String s : wordList) {
             for (int i = 0; i < s.length(); i++) {
-                String pattern = i < s.length() - 1 ? s.substring(0,i) + '*' + s.substring(i + 1)
-                        : s.substring(0,i) + '*'; // remove i from s
+                String pattern = i < s.length() - 1 ? s.substring(0, i) + '*' + s.substring(i + 1)
+                        : s.substring(0, i) + '*'; // remove i from s
                 List<String> list = dic.getOrDefault(pattern, new ArrayList<>());
                 list.add(s);
                 dic.put(pattern, list);
@@ -86,16 +86,16 @@ public class WordLadder {
         vEnd.put(endWord, lEnd);
 
         // start the loop
-        while(!queue.isEmpty() || !qEnd.isEmpty()) {
+        while (!queue.isEmpty() || !qEnd.isEmpty()) {
             // do from the end
             if (!qEnd.isEmpty()) {
                 int sEnd = qEnd.size();
                 while (sEnd > 0) {
                     String curEnd = qEnd.pollFirst();
                     if (visited.containsKey((curEnd))) return visited.get(curEnd) + lEnd - 1;
-                    for(int i = 0; i < curEnd.length(); i++) {
-                        String pattern = i < curEnd.length() - 1 ? curEnd.substring(0,i) + '*' + curEnd.substring(i + 1)
-                                : curEnd.substring(0,i) + '*';
+                    for (int i = 0; i < curEnd.length(); i++) {
+                        String pattern = i < curEnd.length() - 1 ? curEnd.substring(0, i) + '*' + curEnd.substring(i + 1)
+                                : curEnd.substring(0, i) + '*';
                         List<String> list = dic.getOrDefault(pattern, new ArrayList<>());
                         for (String transfer : list) {
                             if (!vEnd.containsKey(transfer)) {
@@ -109,14 +109,14 @@ public class WordLadder {
                 lEnd++;
             }
             // do from the begin
-            if (!queue.isEmpty() ) {
+            if (!queue.isEmpty()) {
                 int size = queue.size();
-                while(size > 0) {
+                while (size > 0) {
                     String cur = queue.pollFirst();
                     if (vEnd.containsKey(cur)) return vEnd.get(cur) + level - 1;
-                    for(int i = 0; i < cur.length(); i++) {
-                        String pattern = i < cur.length() - 1 ? cur.substring(0,i) + '*' + cur.substring(i + 1)
-                                : cur.substring(0,i) + '*';
+                    for (int i = 0; i < cur.length(); i++) {
+                        String pattern = i < cur.length() - 1 ? cur.substring(0, i) + '*' + cur.substring(i + 1)
+                                : cur.substring(0, i) + '*';
                         List<String> list = dic.getOrDefault(pattern, new ArrayList<>());
                         for (String transfer : list) {
                             if (!visited.containsKey(transfer)) {
@@ -138,14 +138,132 @@ public class WordLadder {
     public void test() {
         System.out.println(
                 this.ladderLength("talk", "tail",
-                        Arrays.asList("talk","tons","fall","tail","gale","hall","negs")));
+                        Arrays.asList("talk", "tons", "fall", "tail", "gale", "hall", "negs")));
 
         System.out.println(
                 this.ladderLength("hit", "cog",
-                        Arrays.asList("hot","dot","dog","lot","log","cog")));
+                        Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")));
 
         System.out.println(
                 this.ladderLength("hog", "cog",
+                        Arrays.asList("cog")));
+    }
+
+/*    Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+
+    Only one letter can be changed at a time
+    Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+    Note:
+
+    Return an empty list if there is no such transformation sequence.
+    All words have the same length.
+    All words contain only lowercase alphabetic characters.
+    You may assume no duplicates in the word list.
+    You may assume beginWord and endWord are non-empty and are not the same.
+    Example 1:
+
+    Input:
+    beginWord = "hit",
+    endWord = "cog",
+    wordList = ["hot","dot","dog","lot","log","cog"]
+
+    Output:
+            [
+            ["hit","hot","dot","dog","cog"],
+            ["hit","hot","lot","log","cog"]
+            ]
+    Example 2:
+
+    Input:
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot","dot","dog","lot","log"]
+
+    Output: []
+
+    Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+
+    [
+    [hit, hot, dot, lot, log, dog, cog],
+    [hit, hot, dot, lot, log, cog],
+    [hit, hot, dot, dog, log, cog],
+    [hit, hot, dot, dog, cog],
+    [hit, hot, lot, dot, dog, log, cog],
+    [hit, hot, lot, dot, dog, cog],
+    [hit, hot, lot, log, dog, cog],
+    [hit, hot, lot, log, cog]
+    ]
+
+    */
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        // pre-processing, key is matching pattern and value is words in list
+        Map<String, List<String>> dic = new HashMap<>();
+        for (String s : wordList) {
+            for (int i = 0; i < s.length(); i++) {
+                String pattern = i < s.length() - 1 ? s.substring(0, i) + '*' + s.substring(i + 1)
+                        : s.substring(0, i) + '*'; // remove i from s
+                List<String> list = dic.getOrDefault(pattern, new ArrayList<>());
+                list.add(s);
+                dic.put(pattern, list);
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        dfs(dic, beginWord, endWord, visited, path, result);
+
+        // post processing
+        int shortest = Integer.MAX_VALUE;
+        for (List<String> p : result) {
+            shortest = Math.min(p.size(), shortest);
+        }
+        List<List<String>> shortestRes = new ArrayList<>();
+        for (List<String> p : result) {
+            if (p.size() == shortest) shortestRes.add(p);
+        }
+        return shortestRes;
+    }
+
+    private void dfs(Map<String, List<String>> dic,
+                     String beginWord, String endWord, Set<String> visited,
+                     List<String> path, List<List<String>> result) {
+        visited.add(beginWord);
+        path.add(beginWord);
+        if (beginWord.equals(endWord)) {
+            result.add(new ArrayList<>(path));
+            path.remove(path.size() - 1);
+            visited.remove(beginWord);
+            return;
+        }
+        for (int i = 0; i < beginWord.length(); i++) {
+            String pattern =  i < beginWord.length() - 1 ? beginWord.substring(0, i) + '*' + beginWord.substring(i + 1)
+                    : beginWord.substring(0, i) + '*';
+            List<String> candidates = dic.get(pattern);
+            if (candidates != null) {
+                for (String c : candidates) {
+                    if (!visited.contains(c)) {
+                        dfs(dic, c, endWord, visited, path, result);
+                    }
+                }
+            }
+        }
+        path.remove(path.size() - 1);
+        visited.remove(beginWord);
+    }
+
+    @Test
+    public void testII() {
+        System.out.println(
+                this.findLadders("talk", "tail",
+                        Arrays.asList("talk", "tons", "fall", "tail", "gale", "hall", "negs")));
+
+        System.out.println(
+                this.findLadders("hit", "cog",
+                        Arrays.asList("hot","dot","dog","lot","log","cog")));
+
+        System.out.println(
+                this.findLadders("hog", "cog",
                         Arrays.asList("cog")));
     }
 

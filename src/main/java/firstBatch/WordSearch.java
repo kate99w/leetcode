b30acb7ad831803c7764ccae3f10c7df52627eb7
kate99w <kate99w@gmail.com>
2020-05-18 23:14:24 -0700

    word search

--- src/main/java/firstBatch/WordSearch.java
+++ src/main/java/firstBatch/WordSearch.java
@@ -1,5 +1,7 @@
 package firstBatch;
 
+import org.junit.jupiter.api.Test;
+
 /*
  * 5/8/20
  * leetcode # 79. Word Search
@@ -10,4 +12,82 @@ package firstBatch;
  * level: medium
  */
 public class WordSearch {
+    /*    Given a 2D board and a word, find if the word exists in the grid.
+
+    The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+            Example:
+
+    board =
+            [
+            ['A','B','C','E'],
+            ['S','F','C','S'],
+            ['A','D','E','E']
+            ]
+
+    Given word = "ABCCED", return true.
+    Given word = "SEE", return true.
+    Given word = "ABCB", return false.
+
+    */
+    private final int[][] DIR = new int[][] {{1, 0},{0, 1}, {-1, 0}, {0, -1}};
+
+    public boolean exist(char[][] board, String word) {
+        if (word == null) return false;
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == word.charAt(0))
+                    if (dfs(board, word, 0, i, j, visited)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int level, int row, int col, boolean[][] visited) {
+        if (level >= word.length()) return true;
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col]) return false;
+        visited[row][col] = true;
+        boolean result = false;
+        if (board[row][col] == word.charAt(level)) {
+            for (int[] dir : DIR) {
+                if (dfs(board, word, level + 1, row + dir[0], col + dir[1], visited)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        visited[row][col] = false;
+        return result;
+    }
+
+/*    Given a 2D board and a list of words from the dictionary, find all words in the board.
+
+    Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+
+
+    Example:
+
+    Input:
+    board = [
+            ['o','a','a','n'],
+            ['e','t','a','e'],
+            ['i','h','k','r'],
+            ['i','f','l','v']
+            ]
+    words = ["oath","pea","eat","rain"]
+
+    Output: ["eat","oath"]
+
+
+    Note:
+
+    All inputs are consist of lowercase letters a-z.
+    The values of words are distinct.
+
+
+
+
+    */
 }
